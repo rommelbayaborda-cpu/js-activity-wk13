f947e240352466c41ab733fc3f27f666181ee3f2
markharry <markharry.segurigan@olivarezcollegetagaytay.edu.ph>
2025-11-14 14:59:53 +0800

    week13

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     button.addEventListener("click", function() {
         const name = document.getElementById("name").value;
         const age = document.getElementById("age").value;
-        const email = document.getElementById("email").value;
         const contact = document.getElementById("contact").value;
 
-        const message = `Hello ${name}! You are ${age} years old. Your email is ${email} and your contact number is ${contact}.`;
+        const message = `Hello ${name}! You are ${age} years old. Your contact number is ${contact}.`;
 
         document.getElementById("Result").textContent = message;
     });
